30aadbb608b24461c8fac9117ec2bb7f6657dc38
Adam Feuer <adam@starcat.io>
2023-12-26 12:08:56 -0800

    initial addition
    - updated from chart-plot.py

---
+++ hackeeg-stream-plot.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+
+# from https://stackoverflow.com/a/63677698/431222
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from dash import callback, Dash, dcc, html
+from dash.dependencies import Input, Output
+
+GRAPH_SIZE_IN_ROWS = 100
+
+def main():
+    global app, cols, df
+
+    # code and plot setup
+
+    # settings
+    pd.options.plotting.backend = "plotly"
+    countdown = 20
+
+    # sample dataframe of a wide format
+    np.random.seed(4) 
+    cols = list('a')
+    X = np.random.randn(50,len(cols))  
+    df=pd.DataFrame(X, columns=cols)
+    df.iloc[0]=0;
+
+    # plotly figure
+    fig = df.plot(template = 'plotly_dark')
+
+    app = Dash(__name__)
+    app.layout = html.Div([
+        html.H1("Streaming of random data"),
+                dcc.Interval(
+                id='interval-component',
+                interval=200, # in milliseconds
+                n_intervals=0
+            ),
+        dcc.Graph(id='graph'),
+    ])
+
+    app.run(port = 8069, dev_tools_ui=True, #debug=True,
+            dev_tools_hot_reload =True, threaded=True)
+
+
+# Define callback to update graph
+@callback(
+    Output('graph', 'figure'),
+    [Input('interval-component', "n_intervals")]
+)
+def streamFig(value):
+    
+    global app, cols, df
+    
+    Y = np.random.randn(1,len(cols))  
+    df2 = pd.DataFrame(Y, columns = cols)
+    df = pd.concat([df, df2], ignore_index=True) 
+    df.tail()
+    df3=df.copy()
+    df3 = df3.cumsum()
+    df3_number_of_rows = len(df3)
+    if df3_number_of_rows < GRAPH_SIZE_IN_ROWS:
+         number_of_rows_to_display = df3_number_of_rows
+    else:
+         number_of_rows_to_display = GRAPH_SIZE_IN_ROWS
+    df4 = df3.iloc[-number_of_rows_to_display:]
+    # df4 = df3[-number_of_rows_to_display:]
+    fig = df4.plot(template = 'plotly_dark')
+
+    colors = px.colors.qualitative.Plotly
+    for i, col in enumerate(df4.columns):
+            fig.add_annotation(x=df4.index[-1], y=df4[col].iloc[-1],
+                                   text = str(df4[col].iloc[-1])[:4],
+                                   align="right",
+                                   arrowcolor = 'rgba(0,0,0,0)',
+                                   ax=25,
+                                   ay=0,
+                                   yanchor = 'middle',
+                                   font = dict(color = colors[i]))
+    return(fig)
+
+if __name__ == "__main__":
+    main()
